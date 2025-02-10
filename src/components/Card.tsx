2bbe2ef8541085e6968620e4839847193124f2a5
TaciTa <seoyoung720@gmail.com>
2025-02-10 17:15:06 +0900

    feat : AnimatedCard 생성

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 import styles from "@/styles/Card.module.css";
 
 const Card = () => {
@@ -20,30 +20,37 @@ const Card = () => {
 
 export const AnimatedCard = () => {
   const cardVariants = {
+    rest: {
+      scale: 1,
+    },
     hover: {
       scale: 1.2,
     },
     pressed: {
       scale: 0.8,
     },
-    rest: {
-      scale: 1,
+    drag: {
+      scale: 0.9,
     },
   };
-
+  const controls = useDragControls();
   const [isBack, setIsBack] = useState(true);
 
   const cardFlip = () => setIsBack(!isBack);
 
   return (
     <motion.div
+      variants={cardVariants}
       initial="rest"
       whileHover="hover"
       whileTap="pressed"
-      variants={cardVariants}
+      whileDrag="drag"
+      drag
+      dragControls={controls}
       className={`${styles.card_wrapper} ${
         isBack ? styles.card_back : styles.card_fore
       }`}
+      style={{ touchAction: "none" }}
       onClick={cardFlip}
     />
   );
