bc9a09bacc6ecb740649b6f22e00f9905aee2321
TaciTa <seoyoung720@gmail.com>
2025-02-10 17:15:27 +0900

    feat : motion 컴포넌트들 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { AnimatedCard } from "@/components/Card";
 import styles from "./page.module.css";
+import TableSection from "@/components/TableSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>header</header>
       <main className={styles.main}>
-        <AnimatedCard />
+        <TableSection>
+          <AnimatedCard />
+        </TableSection>
       </main>
       <footer className={styles.footer}>footer</footer>
     </div>
